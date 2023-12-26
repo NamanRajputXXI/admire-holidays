4240c9ec74123ffa244fe198258458fec0d7c23c
Naman Rajput <namanrajput801@gmail.com>
2023-12-26 12:18:05 +0530

    ui:complete the navbar

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+const DropDown = () => {
+  const [isDomesticHovered, setIsDomesticHovered] = useState(false);
+  const [isInternationalHovered, setIsInternationalHovered] = useState(false);
+
+  const handleDomesticHover = () => {
+    setIsDomesticHovered(true);
+    setIsInternationalHovered(false);
+  };
+
+  const handleInternationalHover = () => {
+    setIsDomesticHovered(false);
+    setIsInternationalHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDomesticHovered(false);
+    setIsInternationalHovered(false);
+  };
+  return (
+    <>
+      <div
+        className="font-semibold items-center flex rounded-lg p-2 relative"
+        onMouseEnter={handleDomesticHover}
+        onMouseLeave={handleMouseLeave}
+      >
+        Domestic
+        <RiArrowDropDownLine size={40} />
+        {isDomesticHovered && (
+          <div className="dropdown-content font-light absolute z-50  bg-white p-2 top-12 ">
+            <ul className="">
+              <li>Himachal</li>
+              <li>Goa</li>
+              <li>Andaman</li>
+              <li>Uttarakhand</li>
+              <li>Kashmir</li>
+              <li>Kerala</li>
+              <li>Rajasthan</li>
+              <li>West Bengal</li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div
+        className="font-semibold items-center flex rounded-lg p-2 relative"
+        onMouseEnter={handleInternationalHover}
+        onMouseLeave={handleMouseLeave}
+      >
+        International
+        <RiArrowDropDownLine size={40} />
+        {isInternationalHovered && (
+          <div className="font-light bg-white p-2 dropdown-content absolute top-12 ">
+            <ul>
+              <li>Switzerland</li>
+              <li>Mauritious</li>
+              <li>Thailand</li>
+              <li>Bhutan</li>
+              <li>Nepal</li>
+              <li>Bangladesh</li>
+              <li>Dubai</li>
+              <li>Singapur</li>
+              <li>Malasia</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default DropDown;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,101 +1,118 @@
 "use client";
 import React, { useState } from "react";
-
+import { Ri24HoursFill, RiArrowDropDownLine } from "react-icons/ri";
 import Image from "next/image";
 import Link from "next/link";
+import DropDown from "./DropDown";
+import Button from "./Button";
 const Navbar = () => {
   const [isClick, setIsClick] = useState();
+
   const toggleNavbar = () => {
     setIsClick(!isClick);
   };
+
   return (
     <>
-      <nav className="">
-        <div className="  max-w-7xl py-2 mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <a href="/" className="">
-                  <Image src={"/logo.jpg"} width={150} height={30} alt="Logo" />
-                </a>
+      <div className="fixed z-20 bg-white  top-0 w-screen">
+        <nav className="">
+          <div className="max-w-8xl py-2 mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <a href="/" className="">
+                    <Image
+                      src={"/logo.jpg"}
+                      width={150}
+                      height={30}
+                      alt="Logo"
+                    />
+                  </a>
+                </div>
               </div>
+              <div className="hidden lg:flex items-center justify-center space-x-4">
+                <a href="" className="font-semibold rounded-lg p-2">
+                  Home
+                </a>
+                <DropDown />
+                <a href="" className="font-semibold rounded-lg p-2">
+                  About
+                </a>
+                <a href="" className="font-semibold rounded-lg p-2">
+                  Blog
+                </a>
+                <a href="" className="font-semibold rounded-lg p-2">
+                  Contact
+                </a>
+                {/* <Button /> */}
+              </div>
+              <div className="lg:hidden flex items-center">
+                <button
+                  className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black"
+                  onClick={toggleNavbar}
+                >
+                  {isClick ? (
+                    <svg
+                      className="h-6 w-6"
+                      xmlns=""
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        // strokeLineJoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="h-6 w-6"
+                      xmlns=""
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        // strokeLineJoin="round"
+                        strokeWidth={2}
+                        d="M4 6H16M4 12h16m-7 6h7"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+              <Button display={"lg:flex hidden"} />
             </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center space-x-4"></div>
-              <a href="" className=" rounded-lg p-2">
-                Home
-              </a>
-              <a href="" className=" rounded-lg p-2">
-                About
-              </a>
-              <a href="" className=" rounded-lg p-2">
-                Blog
-              </a>
 
-              <a href="" className="rounded-lg p-2">
-                Contact
-              </a>
-            </div>
+            {isClick && (
+              <div className="lg:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <a href="" className=" font-semibold block rounded-lg p-2">
+                    Home
+                  </a>
+
+                  <DropDown />
+                  <a href="" className="font-semibold block rounded-lg p-2">
+                    About
+                  </a>
+                  <a href="" className="font-semibold block rounded-lg p-2">
+                    Blog
+                  </a>
+                  <a href="" className="font-semibold block rounded-lg p-2">
+                    Contact
+                  </a>
+                  <Button display={"flex"} />
+                </div>
+              </div>
+            )}
           </div>
-          <div className="md:hidden flex items-center">
-            <button
-              className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black"
-              onClick={toggleNavbar}
-            >
-              {isClick ? (
-                <svg
-                  className="h-6 w-6"
-                  xmlns=""
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLineJoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="h-6 w-6"
-                  xmlns=""
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLineJoin="round"
-                    strokeWidth={2}
-                    d="M4 6H16M4 12h16m-7 6h7"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
-        </div>
-        {isClick && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a href="" className=" block rounded-lg p-2">
-                Home
-              </a>
-              <a href="" className="block rounded-lg p-2">
-                About
-              </a>
-              <a href="" className=" block rounded-lg p-2">
-                Blog
-              </a>
-              <a href="" className=" block rounded-lg p-2">
-                Contact
-              </a>
-            </div>
-          </div>
-        )}
-        <hr />
-      </nav>
+
+          <hr />
+        </nav>
+      </div>
     </>
   );
 };

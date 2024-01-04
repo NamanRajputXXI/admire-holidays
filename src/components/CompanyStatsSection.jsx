be7766c75e18786722ccfae662d149ec6d871120
Naman Rajput <namanrajput801@gmail.com>
2024-01-04 15:01:36 +0530

    ui:change the style of company stats section

--- src/components/CompanyStatsSection.jsx
+++ src/components/CompanyStatsSection.jsx
@@ -16,7 +16,16 @@ const CompanyStatsSection = () => {
             className="flex flex-col justify-center items-center gap-10 "
             key={i}
           >
-            <div> {item.icon}</div>
+            <div
+              className="px-8 py-6 flex justify-center items-center bg-gray-600 bg-opacity-20 transform hover:scale-110 transition-transform duration-300"
+              style={{
+                clipPath:
+                  "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
+              }}
+            >
+              {item.icon}
+            </div>
+
             <div className="text-5xl text-white font-medium">
               {item.numbers}
             </div>
--- src/data/comapanyStatsData.js
+++ src/data/comapanyStatsData.js
@@ -4,22 +4,22 @@ import { IoThumbsUpOutline } from "react-icons/io5";
 import { GiStarsStack } from "react-icons/gi";
 const companyStatsData = [
   {
-    icon: <FaPeopleGroup color="white" size={80} />,
+    icon: <FaPeopleGroup color="white" size={40} />,
     numbers: 10000,
     title: "Packages",
   },
   {
-    icon: <FaRoute color="white" size={80} />,
+    icon: <FaRoute color="white" size={40} />,
     numbers: 1000,
     title: "Destination Covered",
   },
   {
-    icon: <IoThumbsUpOutline color="white" size={80} />,
+    icon: <IoThumbsUpOutline color="white" size={40} />,
     numbers: 8,
     title: "Years in Business",
   },
   {
-    icon: <GiStarsStack color="white" size={80} />,
+    icon: <GiStarsStack color="white" size={40} />,
     numbers: 4.9,
     title: "Star Rating",
   },

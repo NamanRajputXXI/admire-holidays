43b87616cff3320d2928a4b2aab38b78c1c0f30d
Naman Rajput <namanrajput801@gmail.com>
2024-01-10 12:57:13 +0530

    ui:change the image location in weekend section

--- src/components/cards/WeekendTripCard.jsx
+++ src/components/cards/WeekendTripCard.jsx
@@ -4,7 +4,7 @@ import { FaLocationDot } from "react-icons/fa6";
 const WeekendTripCard = ({ imageUrl, location, about }) => {
   return (
     <div className="flex flex-col shadow-md gap-3 border-1 rounded-xl  border-gray-200  justify-center">
-      <img src={imageUrl} className="h-72 rounded-t-xl" alt={location} />
+      <img src={imageUrl} className="h-64 rounded-t-xl" alt={location} />
       <h5 className="px-5 text-xl font-medium">{location}</h5>
       <div className="flex pb-4  px-3 text-gray-500 items-center gap-2">
         <FaLocationDot color="#0F9898" size={18} />
--- src/data/weekendTrip.js
+++ src/data/weekendTrip.js
@@ -1,57 +1,49 @@
 // Weekend Trip Data
 const weekendTripData = [
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1583143874828-de3d288be51a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl: `mussoorie.png`,
     location: "Mussoorie",
     about: "Mussoorie,Uttarakhand,India",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1643778765874-23e79f4e38b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGthc2F1bGl8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `kasauli.jpg`,
     location: "Kasauli",
     about: "Kasauli, Himachal Pradesh",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1581247838175-693caf56a86b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8a2FuYXRhbHxlbnwwfHwwfHx8MA%3D%3D",
+    imageUrl: `kanantal.jpg`,
     location: "Kanatal",
     about: "KanatalKaudia, Uttarakhand",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1622611908679-cbeda47d9404?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFyaWR3YXJ8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `haridwar2.jpg`,
     location: "Haridwar",
     about: "Haridwar, Uttarakhand",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1583261429112-e0e7fe037a49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHVzaGthcnxlbnwwfHwwfHx8MA%3D%3D",
+    imageUrl: `pushkar.jpg`,
     location: "Pushkar",
     about: "Pushkar City in Rajasthan",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1610715936287-6c2ad208cdbf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmFpbml0YWx8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `nainital.jpg`,
     location: "Nainital",
     about: "Nainital,Uttarakhand,India",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1583143874828-de3d288be51a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl: `mussoorie2.jpg`,
     location: "Mussoorie",
     about: "Mussoorie,Uttarakhand",
   },
 
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1683442130903-19272738de77?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmVlbXJhbmF8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `neemrana.jpg`,
     location: "Neemrana",
     about: "Neemrana Rajasthan,India",
   },

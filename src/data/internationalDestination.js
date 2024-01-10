2438e00c1cc5c49f6791be574501e8fac1f2882f
Naman Rajput <namanrajput801@gmail.com>
2024-01-10 12:42:32 +0530

    ui:change the images in international carousel

--- src/data/internationalDestination.js
+++ src/data/internationalDestination.js
@@ -1,43 +1,35 @@
 // Destinations Data
 const internationalDestinations = [
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1578556881767-c2cf0bfc9ea3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl: `popularDestinationCarouselBhutan.jpg`,
     title: "Bhutan",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1596895111956-bf1cf0599ce5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmFuZ2xhZGVzaHxlbnwwfHwwfHx8MA%3D%3D",
+    imageUrl: `popularDestinationCarouselBangladesh.jpg`,
     title: "Bangladesh",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1529852572069-2fcbe7599434?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl: `popularDestinationCarouselMauritious.jpg`,
     title: "Mauritious",
   },
   {
-    imageUrl:
-      "https://plus.unsplash.com/premium_photo-1673561276237-4428e63f5804?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3dpdHplcmxhbmR8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `popularDestinationCarouselSwitzerland.jpg`,
     title: "Switzerland",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1575538439014-1b8bc5fcaa1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGR1YmFpfGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselDubai.jpg`,
     title: "Dubai",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1599751229070-854ae5c90869?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fG5lcGFsfGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselNepal.jpg`,
     title: "Nepal",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1629443902634-d902336202b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fHNpbmdhcHVyfGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselSingapore.jpg`,
     title: "Singapore",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1612229370908-5d1648e1d0a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fHRoYWlsYW5kfGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselThailand.jpg`,
     title: "Thailand",
   },
 ];

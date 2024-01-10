bfae116162b9bc084faef5c806b452a06519b6cd
Naman Rajput <namanrajput801@gmail.com>
2024-01-10 12:34:10 +0530

    ui:change the images in domestic carousel

--- src/data/domesticDestination.js
+++ src/data/domesticDestination.js
@@ -1,33 +1,27 @@
 // Destinations Data
 const domesticDestinations = [
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1615966650071-855b15f29ad1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageUrl: `popularDestinationCarouselAndaman.jpg`,
     title: "Andaman",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1558960214-f4283a743867?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGdvYXxlbnwwfHwwfHx8MA%3D%3D",
+    imageUrl: `popularDestinationCarouselGoa.webp`,
     title: "Goa",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1552098933-a5ceb0e5dd91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGthc2htaXJ8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `popularDestinationCarouselKashmir.webp`,
     title: "Kashmir",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1589029661242-fbb01bc4fd1a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHV0dGFyYWtoYW5kfGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselUttarakhand.jpg`,
     title: "Uttarakhand",
   },
   {
-    imageUrl:
-      "https://plus.unsplash.com/premium_photo-1661963369594-9b25cd53be4d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFqYXN0aGFufGVufDB8fDB8fHww",
+    imageUrl: `popularDestinationCarouselRajasthan.webp`,
     title: "Rajasthan",
   },
   {
-    imageUrl:
-      "https://images.unsplash.com/photo-1599831013079-1aa9b2092f08?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHdlc3QlMjBiZW5nYWx8ZW58MHx8MHx8fDA%3D",
+    imageUrl: `popularDestinationCarouselWestBengal.jpg`,
     title: "West Bengal",
   },
 ];

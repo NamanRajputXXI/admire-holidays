a1fd0ae457c39021ccc4148bebb28c47175b2b96
Naman Rajput <namanrajput801@gmail.com>
2024-01-12 17:52:25 +0530

    chore:[WIP] update code

--- server/index.js
+++ server/index.js
@@ -1,26 +1,3 @@
-// server.js
-// const express = require("express");
-// const connectDB = require("./db");
-// const JaipurModel = require("./models/Jaipur");
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// connectDB();
-
-// app.get("/api/jaipur", async (req, res) => {
-//   try {
-//     const jaipurData = await JaipurModel.findOne({ heading: "Jaipur" });
-//     res.json(jaipurData);
-//   } catch (error) {
-//     console.error("Error fetching Jaipur data:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");

120c5f4bbcb942fb0a61c990c59773cfc75d7c83
Naman Rajput <namanrajput801@gmail.com>
2024-02-03 13:22:00 +0530

    fix: build errors

--- src/app/product/nainital/page.jsx
+++ src/app/product/nainital/page.jsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import FormModal from "@/components/FormModal";
 import Navbar from "@/components/Navbar";
 import NainitalProduct from "@/components/product/nainital/NainitalProduct";
 import React from "react";

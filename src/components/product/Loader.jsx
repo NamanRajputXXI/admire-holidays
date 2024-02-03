3b74be3aeb87f9d77158eb1d258e45160322f53f
Naman Rajput <namanrajput801@gmail.com>
2024-02-03 15:41:33 +0530

    ui:changes in loader

--- src/components/product/Loader.jsx
+++ src/components/product/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="text-7xl font-bold flex items-center justify-center h-screen">
+    <div className="sm:text-7xl text-5xl font-bold flex items-center justify-center h-screen">
       {/* Add your loader component or message here */}
       Loading...
     </div>

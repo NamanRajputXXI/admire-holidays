0b55d063f22d4f6bc0dcf7b9b21c281763a22a91
Naman Rajput <namanrajput801@gmail.com>
2024-03-01 12:49:36 +0530

    fix:improve performace of loading data of domestic destionation in dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,3 +1,144 @@
+// import Link from "next/link";
+// import React, { useState, useEffect } from "react";
+// import { RiArrowDropDownLine } from "react-icons/ri";
+
+// const DropDown = () => {
+//   const [isDomesticHovered, setIsDomesticHovered] = useState(false);
+//   const [isInternationalHovered, setIsInternationalHovered] = useState(false);
+//   const [travelData, setTravelData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(
+//           "https://server-deploy-gamma.vercel.app/allStatesData"
+//         );
+//         const data = await response.json();
+//         setTravelData(data);
+//       } catch (error) {
+//         setError("Error fetching data");
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+//   const renderList = () => {
+//     if (loading) {
+//       return <p></p>;
+//     }
+
+//     if (error) {
+//       return <p>{error}</p>;
+//     }
+
+//     return (
+//       <div className="dropdown-content left-0  font-light text-base absolute z-50  bg-white p-8 top-12 ">
+//         <ul className="flex flex-col gap-4">
+//           {Object.entries(travelData).map(([category, destinations]) => {
+//             const capitalizedCategory =
+//               category.charAt(0).toUpperCase() + category.slice(1);
+//             return (
+//               <li key={category}>
+//                 <Link href={`/products/${category}`}>
+//                   {capitalizedCategory}
+//                 </Link>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     );
+//   };
+
+//   const handleDomesticHover = () => {
+//     setIsDomesticHovered(true);
+//     setIsInternationalHovered(false);
+//   };
+
+//   const handleInternationalHover = () => {
+//     setIsDomesticHovered(false);
+//     setIsInternationalHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsDomesticHovered(false);
+//     setIsInternationalHovered(false);
+//   };
+//   return (
+//     <>
+//       <div
+//         className="font-medium cursor-pointer  items-center md:text-lg flex rounded-lg p-2 relative"
+//         onMouseEnter={handleDomesticHover}
+//         onMouseLeave={handleMouseLeave}
+//       >
+//         Domestic
+//         <RiArrowDropDownLine size={30} />
+//         {isDomesticHovered && <div>{renderList()}</div>}
+//       </div>
+//       <div
+//         className="font-medium  cursor-pointer items-center  md:text-lg flex rounded-lg p-2 relative"
+//         onMouseEnter={handleInternationalHover}
+//         onMouseLeave={handleMouseLeave}
+//       >
+//         International
+//         <RiArrowDropDownLine size={30} />
+//         {isInternationalHovered && (
+//           <div className="font-light bg-white text-base p-8 dropdown-content absolute top-12 ">
+//             <ul className="flex flex-col gap-4">
+//               <li>
+//                 <Link href="/product-category/international/switzerland">
+//                   Switzerland
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/mauritious">
+//                   Mauritious
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/thailand">
+//                   Thailand
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/bhutan">
+//                   Bhutan
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/nepal">Nepal</Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/bangladesh">
+//                   Bangladesh
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/dubai">Dubai</Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/singapore">
+//                   Singapore
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/product-category/international/malasia">
+//                   Malasia
+//                 </Link>
+//               </li>
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default DropDown;
+
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
@@ -8,14 +149,25 @@ const DropDown = () => {
   const [travelData, setTravelData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://server-deploy-gamma.vercel.app/allStatesData"
-        );
-        const data = await response.json();
-        setTravelData(data);
+        // Attempt to get cached data from localStorage
+        const cachedData = sessionStorage.getItem("travelData");
+
+        if (cachedData) {
+          setTravelData(JSON.parse(cachedData));
+        } else {
+          const response = await fetch(
+            "https://server-deploy-gamma.vercel.app/allStatesData"
+          );
+          const data = await response.json();
+
+          // Cache the fetched data in localStorage
+          sessionStorage.setItem("travelData", JSON.stringify(data));
+          setTravelData(data);
+        }
       } catch (error) {
         setError("Error fetching data");
       } finally {
@@ -25,6 +177,7 @@ const DropDown = () => {
 
     fetchData();
   }, []);
+
   const renderList = () => {
     if (loading) {
       return <p></p>;
@@ -67,6 +220,7 @@ const DropDown = () => {
     setIsDomesticHovered(false);
     setIsInternationalHovered(false);
   };
+
   return (
     <>
       <div

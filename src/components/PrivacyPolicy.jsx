28c02653dae091fe3e9fbe9bf75c1a834bb265cf
Naman Rajput <namanrajput801@gmail.com>
2024-03-20 15:12:16 +0530

    ui:add privacy policy

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -3,86 +3,62 @@ import React from "react";
 const PrivacyPolicy = () => {
   return (
     <section className="max-w-7xl my-20 px-5 mx-auto flex gap-20 flex-col">
-      <div className="flex flex-col  gap-5">
-        <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Terms and Conditions
-        </h1>
-        <p>
-          This web page represents a legal document that serves as the terms of
-          use for our website (“Terms of Use”), admireholidays.com and any
-          associated mobile application (collectively, “Website”), as owned and
-          operated by 53Take off Pvt limited, Barasingha Private Limited,
-          admireholidays.com., admireholidays FZCO or admireholidays Travel
-          Solutions (“Admireholidays”).
-        </p>
-      </div>
       <div className="flex flex-col gap-5">
         <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Definitions
+          Privacy Policy
         </h1>
         <p>
-          The terms: “us” or “we” or “our” refers to Admireholidays, the owner
-          of the Website.
+          At Admire Holidays, we are committed to safeguarding your privacy
+          while providing you with the best travel and tourism services. With
+          over 7 years of experience, we pride ourselves on offering seamless
+          and stress-free travel experiences for our valued customers. As you
+          explore the beautiful destinations we cover, both domestically and
+          internationally, rest assured that your privacy is of utmost
+          importance to us.
         </p>
         <p>
-          A “Member” is an individual that has registered with our Website to
-          use our Website’s features. A “supplier” is a Member of our Website
-          that is a business offering tours, activities, and other
-          travel-related goods and services to the general public and has
-          registered with our Website to offer their goods/services. We refer to
-          a Member who purchases goods/services from suppliers as a “Customer”
-        </p>
-      </div>
-      <div className="flex flex-col gap-5">
-        <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Our Relationship to You
-        </h1>
-        <p>
-          Admireholidays is strictly a venue that does NOT enter into any other
-          relationship with you, other than that of an independent contractor.
-          Our Legal Terms in no way create any agency, partnership, joint
-          venture, employee-employer or franchisor-franchisee relationship
-          between you and Thrillophilia, other Users, or our affiliates.
-        </p>
-      </div>
-      <div className="flex flex-col gap-5">
-        <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Information From Children
-        </h1>
-        <p>
-          We do not knowingly solicit, collect or retain information from any
-          individuals under the age of 13.
-        </p>
-      </div>
-      <div className="flex flex-col gap-5">
-        <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Supplier and Sales of Goods/Services
-        </h1>
-        <p>
-          As a supplier, you can upload information about your business,
-          including address, phone, hours of operation, and other such relevant
-          information. By uploading such information, you warrant to
-          Admireholidays that such information is true to the best of your
-          knowledge and is not in any way malicious, libellous, or otherwise
-          illegal for you to transmit to our Website.
+          We collect personal information from you solely for the purpose of
+          delivering our travel services efficiently and effectively. This may
+          include details such as your name, contact information, travel
+          preferences, and payment details. We assure you that this information
+          is securely stored and only used for the purpose of fulfilling your
+          travel requirements.
         </p>
         <p>
-          As a supplier, if you contact Customers outside our Website with
-          correspondence, you agree to abide by such Customers’ requests to be
-          removed from future correspondence. If we receive complaints that you
-          are failing to do so, we reserve the right to terminate your Profile
-          at our sole discretion with no notice to you.
+          Your personal information will not be shared, sold, or rented to any
+          third parties for marketing purposes without your explicit consent.
+          However, we may need to disclose your information to our trusted
+          partners and service providers who assist us in delivering our travel
+          services, such as airlines, hotels, tour operators, and transportation
+          providers. Rest assured, these third parties are bound by strict
+          confidentiality agreements and are only permitted to use your
+          information to fulfill the services you have requested.
         </p>
-      </div>
-      <div className="flex flex-col gap-5">
-        <h1 className="md:text-3xl md:font-bold text-2xl font-semibold">
-          Indemnification
-        </h1>
         <p>
-          You agree to indemnify, defend and hold harmless Admireholidays and
-          our partners, agents, officers, directors, employees, subcontractors,
-          successors, assigns, third party suppliers, attorneys, advertisers,
-          and affiliates, from any liability, loss, claim and expense, including
+          We may also use your personal information to communicate with you
+          about your travel bookings, provide updates on our services, and
+          inform you about special offers or promotions that may be of interest
+          to you. You have the right to opt out of receiving such communications
+          at any time.
+        </p>
+        <p>
+          Furthermore, we employ industry-standard security measures to protect
+          your personal information from unauthorized access, disclosure,
+          alteration, or destruction. However, please be aware that no method of
+          transmission over the internet or electronic storage is 100% secure,
+          and we cannot guarantee absolute security.
+        </p>
+        <p>
+          By using our website and availing of our services, you consent to the
+          collection and use of your personal information as outlined in this
+          Privacy Policy. We reserve the right to update or modify this Privacy
+          Policy at any time, and any changes will be effective immediately upon
+          posting on our website.If you have any questions or concerns regarding
+          our Privacy Policy or the handling of your personal information,
+          please feel free to contact us. Your privacy and satisfaction are
+          paramount to us, and we are here to address any inquiries or feedback
+          you may have. Thank you for choosing Admire Holidays for your travel
+          needs.
         </p>
       </div>
     </section>

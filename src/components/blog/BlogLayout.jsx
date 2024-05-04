6c7201a81d7af11f4aac7f39f2a62617c2cfbeb7
Naman Rajput <namanrajput801@gmail.com>
2024-05-04 10:50:36 +0530

    ui:change the ui of the blog page

--- src/components/blog/BlogLayout.jsx
+++ src/components/blog/BlogLayout.jsx
@@ -2,8 +2,7 @@
 import blogPagedata from "@/data/blogPageData";
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
-import { FaRegFolderOpen } from "react-icons/fa";
-import BlogPageCard2 from "../cards/BlogPageCard2";
+
 import BlogPageCard from "../cards/BlogPageCard";
 const BlogLayout = ({ params }) => {
   const [result, setResult] = useState("Subscribe");
@@ -47,180 +46,113 @@ const BlogLayout = ({ params }) => {
   };
 
   return (
-    <section className="max-w-7xl pt-20 flex items-center justify-center mx-auto px-5 my-20">
-      <div className="flex w-full md:flex-row flex-col justify-between gap-10 ">
-        <div className=" md:w-[70%] w-full flex flex-col gap-20 ">
-          <BlogPageCard
-            imageUrl={post.imageUrl}
-            title={post.title}
-            detail={post.detail}
-            comment={post.comment}
-            date={post.date}
-          />
-        </div>
-        <div className="md:w-[30%] w-full flex flex-col gap-5">
-          <div className="flex flex-col gap-5 h-fit border-[1px] border-gray-300 px-4  py-6 rounded-xl ">
-            <h5 className="text-lg font-medium">Recent Post</h5>
-            <hr />
-            <div className="flex flex-col gap-5">
-              <p className="font-light text-base">
-                The Majestic Tiger Falls: Nature’s Hidden Gem
-              </p>
-              <p className="font-light text-base">
-                Pachmarhi: The Queen of Satpura, A Hidden Gem in Central India
-              </p>
-              <p className="font-light text-base">
-                Exploring the Wilderness: Discovering the Riches of Kaziranga
-                National Park
-              </p>
-              <p className="font-light text-base">
-                Unveiling the Enigmatic Charm of Pithoragarh
-              </p>
-              <p className="font-light text-base">
-                Konark: Exploring the Architectural Marvel of the Sun Temple
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center relative min-w-[100%] ">
-            <input
-              type="text"
-              placeholder="search..."
-              className="min-w-[100%] rounded-xl  p-3 border-[1px] border-gray-300"
-            />
-            <CiSearch
-              size={40}
-              className="absolute cursor-pointer right-2"
-              color="gray"
+    <section>
+      <img
+        src={post.imageUrl}
+        className="w-full h-full px-5 lg:px-16  md:px-8 sm:pt-32 pt-[116px] lg:pt-44"
+        alt=""
+      />
+      <div className="max-w-7xl  flex items-center justify-center mx-auto px-5 my-20">
+        <div className="flex w-full md:flex-row flex-col justify-between gap-10 ">
+          <div className=" md:w-[70%] w-full flex flex-col gap-20 ">
+            <BlogPageCard
+              imageUrl={post.imageUrl}
+              title={post.title}
+              detail={post.detail}
+              comment={post.comment}
+              date={post.date}
+              author={post.author}
+              imageUrl1={post.imageUrl1}
+              imageUrl2={post.imageUrl2}
+              content={post.content}
             />
           </div>
-          <div className="flex flex-col gap-5 h-fit border-[1px] border-gray-300 px-4  py-6 rounded-xl ">
-            <h5 className="text-lg font-medium">Blog Categories</h5>
-            <hr />
-            <div className="flex flex-col gap-5">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">Food</p>
-                </div>
-                <p className="font-light text-base">(1)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">
-                    Imrove Life
-                  </p>
-                </div>
-                <p className="font-light text-base cursor-pointer">(8)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">Life</p>
-                </div>
-                <p className="font-light text-base cursor-pointer">(2)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">
-                    Off topic{" "}
-                  </p>
-                </div>
-                <p className="font-light text-base">(2)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">
-                    Productivity
-                  </p>
-                </div>
-                <p className="font-light text-base">(2)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">
-                    Self Decipline
-                  </p>
-                </div>
-                <p className="font-light text-base cursor-pointer">(1)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">Sport</p>
-                </div>
-                <p className="font-light text-base">(1)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">
-                    Tips and Tricks
-                  </p>
-                </div>
-                <p className="font-light text-base">(7)</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <FaRegFolderOpen size={20} color="#6b7280" />
-                  <p className="font-light text-base cursor-pointer">Travel</p>
-                </div>
-                <p className="font-light text-base cursor-pointer">(11)</p>
+          <div className="md:w-[30%] w-full flex flex-col gap-5">
+            <div className="flex flex-col gap-5 h-fit border-[1px] border-gray-300 px-4  py-6 rounded-xl ">
+              <h5 className="text-lg font-medium">Recent Post</h5>
+              <hr />
+              <div className="flex flex-col gap-5">
+                <p className="font-light text-base">
+                  The Majestic Tiger Falls: Nature’s Hidden Gem
+                </p>
+                <p className="font-light text-base">
+                  Pachmarhi: The Queen of Satpura, A Hidden Gem in Central India
+                </p>
+                <p className="font-light text-base">
+                  Exploring the Wilderness: Discovering the Riches of Kaziranga
+                  National Park
+                </p>
+                <p className="font-light text-base">
+                  Unveiling the Enigmatic Charm of Pithoragarh
+                </p>
+                <p className="font-light text-base">
+                  Konark: Exploring the Architectural Marvel of the Sun Temple
+                </p>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col gap-5 h-fit  text-white bg-[#1A1A3D;] justify-center items-center  px-4  py-6 rounded-xl ">
-            <img
-              src="https://admireholidays.com/wp-content/uploads/2022/07/subscribe-mail.png"
-              alt=""
-            />
-            <h5 className="text-[#ef4444]  text-center">
-              SUBSCRIBE TO OUR NEWS LETTER
-            </h5>
-            <h1 className="font-semibold">
-              GET THE LATEST NEWS, UPDATES AND LATEST OFFERS
-            </h1>
-            <form
-              onSubmit={submitForm}
-              className="flex flex-col gap-5 items-center justify-center"
-            >
+            <div className="flex items-center relative min-w-[100%] ">
               <input
-                type="email"
-                placeholder="Enter your email"
-                name="mail"
-                className={`min-w-[100%] rounded-xl text-black p-3 border-[1px] bg-white ${
-                  !formValid && "border-red-500"
-                }`}
+                type="text"
+                placeholder="search..."
+                className="min-w-[100%] rounded-xl  p-3 border-[1px] border-gray-300"
               />
-              {!formValid && (
-                <p className="text-red-500">Please enter a valid email</p>
-              )}
-              <button className="w-full h-12 rounded-xl bg-[#ef4444]">
-                {result}
-              </button>
-            </form>
-          </div>
-          <div className="flex flex-col gap-5 h-fit border-[1px] border-gray-300 px-4  py-6 rounded-xl ">
-            <h5 className="text-lg font-medium">Popular Tags</h5>
-            <hr />
-            <div className="flex flex-col  gap-5">
-              <div className="flex  gap-2">
-                <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
-                  Backpacking Clothes
+              <CiSearch
+                size={40}
+                className="absolute cursor-pointer right-2"
+                color="gray"
+              />
+            </div>
+
+            <div className="flex flex-col gap-5 h-fit  text-white bg-[#1A1A3D;] justify-center items-center  px-4  py-6 rounded-xl ">
+              <img
+                src="https://admireholidays.com/wp-content/uploads/2022/07/subscribe-mail.png"
+                alt=""
+              />
+              <h5 className="text-[#ef4444]  text-center">
+                SUBSCRIBE TO OUR NEWS LETTER
+              </h5>
+              <h1 className="font-semibold">
+                GET THE LATEST NEWS, UPDATES AND LATEST OFFERS
+              </h1>
+              <form
+                onSubmit={submitForm}
+                className="flex flex-col gap-5 items-center justify-center"
+              >
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  name="mail"
+                  className={`min-w-[100%] rounded-xl text-black p-3 border-[1px] bg-white ${
+                    !formValid && "border-red-500"
+                  }`}
+                />
+                {!formValid && (
+                  <p className="text-red-500">Please enter a valid email</p>
+                )}
+                <button className="w-full h-12 rounded-xl bg-[#ef4444]">
+                  {result}
+                </button>
+              </form>
+            </div>
+            <div className="flex flex-col gap-5 h-fit border-[1px] border-gray-300 px-4  py-6 rounded-xl ">
+              <h5 className="text-lg font-medium">Popular Tags</h5>
+              <hr />
+              <div className="flex flex-col  gap-5">
+                <div className="flex  gap-2">
+                  <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
+                    Backpacking Clothes
+                  </div>
+                  <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
+                    Place
+                  </div>
                 </div>
-                <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
-                  Place
-                </div>
-              </div>
-              <div className="flex gap-2">
-                <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
-                  Story
-                </div>
-                <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
-                  Travel
+                <div className="flex gap-2">
+                  <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
+                    Story
+                  </div>
+                  <div className="px-4 py-3 font-light cursor-pointer w-fit flex bg-gray-300 rounded-xl items-center justify-center">
+                    Travel
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/cards/BlogPageCard.jsx
+++ src/components/cards/BlogPageCard.jsx
@@ -3,7 +3,17 @@ import { CiCalendarDate } from "react-icons/ci";
 import { FaRegCommentDots, FaRegFolderOpen } from "react-icons/fa";
 import "../../styles/custom.css"; // Import CSS file for custom styles
 
-const BlogPageCard = ({ imageUrl, title, date, comment, detail }) => {
+const BlogPageCard = ({
+  imageUrl,
+  title,
+  date,
+  comment,
+  detail,
+  author,
+  imageUrl1,
+  imageUrl2,
+  content,
+}) => {
   const [expanded, setExpanded] = useState(false);
 
   const toggleExpanded = () => {
@@ -12,38 +22,30 @@ const BlogPageCard = ({ imageUrl, title, date, comment, detail }) => {
 
   return (
     <div className="blog-page-card">
-      <img src={imageUrl} className="card-image" alt="" />
       <h1 className="card-title">{title}</h1>
-      <div className="card-metadata sm:gap-5 gap-3 ">
-        <div className="metadata-item">
-          <CiCalendarDate size={30} color="#ef4444" />
-          <p>{date}</p>
+      <div className="flex flex-col ">
+        <div className="font-bold ">
+          <p>by {author}</p>
         </div>
-        <div className="metadata-item">
-          <FaRegFolderOpen size={30} color="#ef4444" />
-          <p>Travel</p>
-        </div>
-        <div className="metadata-item">
-          <FaRegCommentDots size={30} color="#ef4444" />
-          <p>{comment}</p>
+        <div className="text-sm">
+          <p>published on {date}</p>
         </div>
       </div>
-      <div className="card-detail">
-        <p>
-          <strong>Introduction</strong>:
-        </p>
-        <p className={`detail-text ${expanded ? "expanded" : ""}`}>{detail}</p>
+      <div>
+        <p>{detail}</p>
       </div>
-      {!expanded && (
-        <button className="toggle-button" onClick={toggleExpanded}>
-          Read More
-        </button>
-      )}
-      {expanded && (
-        <button className="toggle-button" onClick={toggleExpanded}>
-          Read Less
-        </button>
-      )}
+      <div className="flex flex-col py-10 gap-5">
+        <img src={imageUrl1} className="w-full h-full" alt="" />
+        <div className="pt-5">
+          {content.map((item, i) => (
+            <div className="flex py-5 flex-col gap-1" key={i}>
+              <h1 className="text-xl font-bold">{item.subheading}</h1>
+              <p>{item.text}</p>
+              <img src={item.imageUrl} className="w-full h-full" alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
